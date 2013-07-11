1dffab772f4bef2bd90e190220f59aaf7f026964
lgbarr <lgbarr@gmail.com>
2013-07-11 22:24:06 +0000

    Now reading the bitstart index.html

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send('Hello World2!');
-  var buffer = new Buffer(40);
+  var buffer = new Buffer(4096);
   buffer = fs.readFileSync('index.html');
   var string = "";
   string = buffer.toString();
