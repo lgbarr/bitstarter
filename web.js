60d14e3518493a4ce519e8fda1cf689618189c9b
lgbarr <lgbarr@gmail.com>
2013-07-11 21:55:15 +0000

    Now reading index.html

--- web.js
+++ web.js
@@ -3,7 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+//  response.send('Hello World2!');
+  var buffer = new Buffer(40);
+  buffer = fs.readFileSync('index.html');
+  var string = "";
+  string = buffer.toString();
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
